64530aeca49dfad731dc6c0202ae1ca3445026ea
vectorheart <charlesnev0706@gmail.com>
2026-01-14 13:31:36 +0800

    Week 2 update backend

--- activities/backend/index.js
+++ activities/backend/index.js
@@ -3,6 +3,31 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+//get -> display name, var name="Charles",
+//post -> logic, if username="charles" password="Pass123" success else failed;
+
+app.use(express.json());
+
+app.get("/getName", (req, res) => {
+  var name = "Charles";
+  res.status(200).json(name);
+});
+
+app.post("/login", (req, res) => {
+  var { username, password } = req.body;
+  if (username == "charles" && password == "Pass123") {
+    res.status(200).json({
+      message: "Login Successfull",
+      status: "success",
+    });
+  } else {
+    res.status(403).json({
+      message: "Invalid username or password.",
+      status: "failed",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
