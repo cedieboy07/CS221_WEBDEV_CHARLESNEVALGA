88a418f50e9f2f2e46b9742ba7af9f5db4674fdb
charles-ciit <charlesnev0706@gmail.com>
2026-01-21 13:35:22 +0800

    Week 3 user reg-log-logout

---
+++ activities/backend/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+//Middleware
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+export default mongoose.model("user", userSchema);
